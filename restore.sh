9a78d7ab110fbf54b1a7cdbb058547d5eb23ee95
Aleš Roubíček <ales@roubicek.name>
2017-07-11 13:05:51 +0200

    Added restore.sh [skip ci]

---
+++ restore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -n "${LAST_BACKUP}" ]; then
+  # Find last backup file
+  : ${LAST_BACKUP:=$(aws s3 --region $AWS_DEFAULT_REGION ls $S3_PATH | awk -F " " '{print $4}' | grep ^$DB_NAME | sort -r | head -n1)}
+fi
+
+# Download backup from S3
+aws s3 --region $AWS_DEFAULT_REGION cp $S3_PATH $LAST_BACKUP || (echo "Failed to download backup from S3"; exit)
+
+# Restore backup
+
+PGPASSWORD="$DB_PASS" pg_restore -j 4 -Fc -c -d "$DB_NAME" -h "$DB_HOST" -U "$DB_USER" "$LAST_BACKUP"
+
+# If a post extraction command is defined, run it
+if [ -n "$AFTER_RESTORE_CMD" ]; then
+	eval "$AFTER_RESTORE_CMD" || exit
+fi
