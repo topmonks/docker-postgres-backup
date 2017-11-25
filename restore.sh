30a0f9370680b4e185502e361c404875c12e8a04
Aleš Roubíček <ales@roubicek.name>
2017-11-25 18:28:49 +0100

    Find latest backup

--- restore.sh
+++ restore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-## ${LAST_BACKUP:=$(aws s3 --region $AWS_DEFAULT_REGION ls $S3_PATH | awk -F " " '{print $4}' | grep ^$DB_NAME | sort -r | head -n1)}
+: ${LAST_BACKUP:=$(aws s3 --region "$AWS_DEFAULT_REGION" ls "$S3_PATH/" | awk -F " " '{print $4}' | grep ^$DB_NAME | sort -r | head -n1)}
 
 # Download backup from S3
 echo "s3 path: $S3_PATH/$LAST_BACKUP"
-aws s3 --region $AWS_DEFAULT_REGION cp $S3_PATH/$LAST_BACKUP $LAST_BACKUP || (echo "Failed to download backup from S3"; exit)
+aws s3 --region "$AWS_DEFAULT_REGION" cp "$S3_PATH/$LAST_BACKUP" "$LAST_BACKUP" || (echo "Failed to download backup from S3"; exit)
 
 # Restore backup
 
@@ -13,5 +13,5 @@ PGPASSWORD="$DB_PASS" pg_restore -j 4 -Fc -c -d "$DB_NAME" -h "$DB_HOST" -U "$DB
 
 # If a post extraction command is defined, run it
 if [ -n "$AFTER_RESTORE_CMD" ]; then
-	eval "$AFTER_RESTORE_CMD" || exit
+  eval "$AFTER_RESTORE_CMD" || exit
 fi
